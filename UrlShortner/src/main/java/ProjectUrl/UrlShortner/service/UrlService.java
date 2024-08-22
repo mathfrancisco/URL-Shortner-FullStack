f082613099a8c6528ce800c63dd41509a647e52c
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-08-22 08:34:56 -0300

    Atualizar o UrlService.java

--- UrlShortner/src/main/java/ProjectUrl/UrlShortner/service/UrlService.java
+++ UrlShortner/src/main/java/ProjectUrl/UrlShortner/service/UrlService.java
@@ -1,29 +1,33 @@
 package ProjectUrl.UrlShortner.service;
 
-
 import ProjectUrl.UrlShortner.modal.Url;
 import ProjectUrl.UrlShortner.repository.UrlRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static ProjectUrl.UrlShortner.logic.GenerateShortUrl.getShortUrl;
 import static ProjectUrl.UrlShortner.logic.GenerateShortUrl.isUrlValid;
 
-
 @Service
 public class UrlService {
+    private static final Logger logger = LoggerFactory.getLogger(UrlService.class);
+
     @Autowired
     private UrlRepository urlRepository;
 
-
+    @Transactional(readOnly = true)
     public String getOriginalUrl(String id) {
         return urlRepository.findByShortUrl(id);
     }
 
+    @Transactional
     public Url generateShortUrl(String url) {
-        if(! isUrlValid(url)) {
-            System.out.println("URL is not valid");
-            return null;
+        if (!isUrlValid(url)) {
+            logger.error("Invalid URL: {}", url);
+            throw new IllegalArgumentException("URL is not valid");
         }
 
         Url urlObject = new Url();
