1eaa18da0e1fd0eaa660e881a2a944faa25aaa2b
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-08-22 08:33:55 -0300

    Atualizar o UrlController.java

--- UrlShortner/src/main/java/ProjectUrl/UrlShortner/controller/UrlController.java
+++ UrlShortner/src/main/java/ProjectUrl/UrlShortner/controller/UrlController.java
@@ -3,6 +3,7 @@ package ProjectUrl.UrlShortner.controller;
 import ProjectUrl.UrlShortner.modal.Url;
 import ProjectUrl.UrlShortner.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,19 @@ public class UrlController {
 
     @GetMapping("/{id}")
     public String getOriginalUrl(@PathVariable String id) {
-        return urlService.getOriginalUrl(id);
+        String originalUrl = urlService.getOriginalUrl(id);
+        if (originalUrl == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "URL not found");
+        }
+        return originalUrl;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Url generateShortUrl(@RequestBody String url) {
+        if (url == null || url.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "URL cannot be empty");
+        }
         return urlService.generateShortUrl(url);
     }
-
 }
